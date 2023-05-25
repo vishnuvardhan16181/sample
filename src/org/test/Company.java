a2e8c9aa4d69bd4d9d2b976d0045245addb745c1
vishnuvardhan16181 <vishnuvardhan16181@gmail.com>
2023-05-25 13:34:53 +0530

    sample project

--- src/org/test/Company.java
+++ src/org/test/Company.java
@@ -1,8 +1,13 @@
 package org.test;
 
 public class Company {
+     public void companyname(){
+    	 System.out.println("tcs");
+     }
 	public static void main(String[] args) {
 		System.out.println("vishnu");
+		
+		
 	}
 
 }
